de699834512acea07906061b08bde2d7e1b5ce88
yuuretsu <yurafrozen@gmail.com>
2021-12-14 12:20:08 +0500

    remove files on setup-typescript

--- scripts/setup-typescript.js
+++ scripts/setup-typescript.js
@@ -7,45 +7,49 @@ const { argv } = require('process');
 /** @type {string} */
 const projectRoot = argv[2] || path.join(__dirname, "..");
 
-// // rename src/index.js -> src.index.ts
+// rename src/index.js -> src.index.ts
 
-// fs.renameSync(
-//   path.join(projectRoot, "src", "lib", "index.js"),
-//   path.join(projectRoot, "src", "lib", "index.ts")
-// );
-// fs.renameSync(
-//   path.join(projectRoot, "src", "index.js"),
-//   path.join(projectRoot, "src", "index.ts")
-// );
-
-
-// // add global.d.ts
-
-// fs.renameSync(
-//   path.join(projectRoot, "scripts", "global.d.ts"),
-//   path.join(projectRoot, "src", "global.d.ts")
-// );
-
-// // add tsconfig.json
-
-// fs.renameSync(
-//   path.join(projectRoot, "scripts", "tsconfig.json"),
-//   path.join(projectRoot, "tsconfig.json")
-// );
+fs.renameSync(
+  path.join(projectRoot, "src", "lib", "index.js"),
+  path.join(projectRoot, "src", "lib", "index.ts")
+);
+fs.renameSync(
+  path.join(projectRoot, "src", "index.js"),
+  path.join(projectRoot, "src", "index.ts")
+);
 
 
+// add global.d.ts
 
-// // edit package.json
+fs.renameSync(
+  path.join(projectRoot, "scripts", "global.d.ts"),
+  path.join(projectRoot, "src", "global.d.ts")
+);
 
-// fs.renameSync(
-//   path.join(projectRoot, "scripts", "package.json"),
-//   path.join(projectRoot, "package.json")
-// );
+// add tsconfig.json
+
+fs.renameSync(
+  path.join(projectRoot, "scripts", "_tsconfig.json"),
+  path.join(projectRoot, "tsconfig.json")
+);
 
 
-// // edit rollup.config.js
 
-// fs.renameSync(
-//   path.join(projectRoot, "scripts", "rollup.config.js"),
-//   path.join(projectRoot, "rollup.config.js")
-// );
+// edit package.json
+
+fs.renameSync(
+  path.join(projectRoot, "scripts", "package.json"),
+  path.join(projectRoot, "package.json")
+);
+
+
+// edit rollup.config.js
+
+fs.renameSync(
+  path.join(projectRoot, "scripts", "rollup.config.js"),
+  path.join(projectRoot, "rollup.config.js")
+);
+
+// remove scripts
+
+fs.rmSync(path.join(projectRoot, "scripts"), { recursive: true, force: true });
