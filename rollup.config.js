3fa0ded8229de7e447624d155f3de7a900bce453
yuuretsu <yurafrozen@gmail.com>
2021-12-15 10:13:26 +0500

    fix input format

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: "./src/index.ts",
+  input: "./src/index.js",
   output: {
     file: "./public/build/bundle.js",
     format: 'iife',
